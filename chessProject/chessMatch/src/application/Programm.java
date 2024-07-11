e831c86e21293e55ad055f49bcc374e96852056f
Felipe Vida <fvida34@gmail.com>
2024-07-11 11:44:34 -0300

    Classe Position

---
+++ chessProject/chessMatch/src/application/Programm.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(3, 4);
+		System.out.println(position);
+		
+	}
+
+}
