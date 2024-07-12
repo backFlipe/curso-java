9132a2ca19c98efb9261090db86bda2f72fc5b0d
Felipe Vida <fvida34@gmail.com>
2024-07-12 14:12:32 -0300

    Apresentação de peças capturadas

--- chessProject/chessMatch/src/application/Programm.java
+++ chessProject/chessMatch/src/application/Programm.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -16,10 +18,12 @@ public class Programm {
 		
 		ChessMatch chessMatch = new ChessMatch();
 		
+		List <ChessPiece> captured = new ArrayList<>();
+		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -33,7 +37,11 @@ public class Programm {
 				System.out.println("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				
-				ChessPiece captured = chessMatch.performChessMove(source, target);
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
