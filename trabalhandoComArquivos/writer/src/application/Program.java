ce63e8bd216483fe3b2fc5d58d0e5e7aa635a3ce
Felipe Vida <fvida34@gmail.com>
2024-07-15 14:29:21 -0300

    Algoritmo que adiciona texto a um arquivo já existente

--- trabalhandoComArquivos/writer/src/application/Program.java
+++ trabalhandoComArquivos/writer/src/application/Program.java
@@ -8,13 +8,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		//CRIA UM ARQUIVO DE TEXTO newFile.txt NO DIRETÓRIO ESPECIFICADO COM O CONTEÚDO DO VETOR lines
+		//ADICIONA O CONTEÚDO DO VETOR lines NO ARQUIVO DE TEXTO newFile.txt
 		
 		String [] lines = new String[] {"Mr. Mercedes", "Achados e Perdidos", "Último Turno"};
 		
 		String path = "/home/back-flipe/eclipse-workspace/curso-java/trabalhandoComArquivos/writer/newFile.txt";
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
 			for (String line : lines) {
 				bw.write(line);
 				bw.newLine();
