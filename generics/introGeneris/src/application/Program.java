04f3dc2ab4765080e80bfbe93e27265f16ab023f
Felipe Vida <fvida34@gmail.com>
2024-07-19 14:11:56 -0300

    Solução com classe generica

--- generics/introGeneris/src/application/Program.java
+++ generics/introGeneris/src/application/Program.java
@@ -2,29 +2,33 @@ package application;
 
 import java.util.Scanner;
 
-import model.services.PrintStringService;
+import model.services.PrintService;
 
 public class Program {
-	
-	//PARA ESSA SOLUÇÃO FOI NECESSÁRIA A CRIAÇÃO DE DUAS CLASSES, UMA PARA CADA TIPO GENÉRICO
 
 	public static void main(String[] args) {
 		
+		/*PARA ESSA SOLUÇÃO FOI UTILIZADA UMA CLASSE GENÉRICA "PrintService<T>", O QUE PERMITE O
+		 *REUSO DA CLASSE, GARANTE TYPE SAFETY E AUMENTA A PERFORMANCE 
+		 */
+		
 		Scanner sc = new Scanner(System.in);
 		
-		PrintStringService ps = new PrintStringService();
+		PrintService<Integer> ps = new PrintService<>();
 		
 		System.out.println("How many values? ");
 		int n = sc.nextInt();
 
 		
 		for (int i = 0; i < n; i++) {
-			String value = sc.next();
+			int value = sc.nextInt();
 			ps.addValue(value);
 		}
 		
 		ps.print();
 		System.out.println("First: " + ps.first());
+		
+		sc.close();
 	}
 
 }
